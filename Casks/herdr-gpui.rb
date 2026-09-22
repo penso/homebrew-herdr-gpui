70791e9b73870b51dac52c0235f6303c31608844
Herdr GPUI release <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-22 15:29:39 +0000

    chore: release herdr-gpui 20260922.3

--- Casks/herdr-gpui.rb
+++ Casks/herdr-gpui.rb
@@ -1,6 +1,6 @@
 cask "herdr-gpui" do
-  version "20260922.2"
-  sha256 "53db0942483eefd44a555951dbee58985e8811b82cf1409fc62e759587de5ff3"
+  version "20260922.3"
+  sha256 "44631a21b39f970474e3096d21df40b536c6817b41e20784b746a682573355cc"
 
   url "https://github.com/penso/herdr-gpui/releases/download/v#{version}/Herdr-#{version}-universal-apple-darwin.dmg"
   name "Herdr"
